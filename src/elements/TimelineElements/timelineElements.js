5de14a016466895e6859ef6c012b17494383e459
sid-ahmedmoussi0 <moussisidahmed0@gmail.com>
2024-04-02 14:08:21 +0200

    Add formation

--- src/elements/TimelineElements/timelineElements.js
+++ src/elements/TimelineElements/timelineElements.js
@@ -1,7 +1,14 @@
-
 const timelineElements = [
   {
     id: 1,
+    title: "Formation - Développeur Java",
+    location: "Wild Code School",
+    mission: "",
+    date: "15/04/2024 - 12/07/2024",
+    icon: "work",
+  },
+  {
+    id: 2,
     title: "Master Ingénierie du Web",
     location: "ESGI, Paris",
     descritpion: "",
@@ -9,17 +16,19 @@ const timelineElements = [
     icon: "school",
   },
   {
-    id: 2,
+    id: 3,
     title: "Alternance - Développeur Full Stack",
     location: "Bucares, Évry-Courcouronnes ",
-    mission: "Réalisation d'une application web en utilisant le framework Spring et diverses bibliothèques JS (React JS, Redux et Axios)",
-    secondMission: "Réalisation d'un Proof of concept en utilisant le framework Spring et diverses bibliothèques JS (React JS, Redux et Axios)",
+    mission:
+      "Réalisation d'une application web en utilisant le framework Spring et diverses bibliothèques JS (React JS, Redux et Axios)",
+    secondMission:
+      "Réalisation d'un Proof of concept en utilisant le framework Spring et diverses bibliothèques JS (React JS, Redux et Axios)",
     thirdMission: "Optimisation SEO d'un site web",
     date: "12/09/2022 - 12/09/2023",
     icon: "work",
   },
   {
-    id: 3,
+    id: 4,
     title: "Licence professionnelle Métiers de l’Informatique Applications Web",
     location: "Université d’Évry, Évry-Courcouronnes",
     descritpion: "",
@@ -27,15 +36,16 @@ const timelineElements = [
     icon: "school",
   },
   {
-    id: 4,
+    id: 5,
     title: "Stage - Développeur Web",
     location: "Bucares, Évry-Courcouronnes ",
     mission: "Création de sites internet via un outil de gestion",
     secondMission: "Développement en JSON",
     date: "31/05/2021 - 08/07/2021 (6 semaines)",
     icon: "work",
-  },  {
-    id: 5,
+  },
+  {
+    id: 6,
     title: "BTS Systèmes Numériques option Informatique et Réseaux",
     location: "Lycée Edmond Michelet, Arpajon",
     descritpion: "",
