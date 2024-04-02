f505d1e18cfb1bf6b6955565cd37f08042d7eae0
sid-ahmedmoussi0 <moussisidahmed0@gmail.com>
2024-04-02 14:11:50 +0200

    Update CV file name

--- src/components/CVButton/CVButton.jsx
+++ src/components/CVButton/CVButton.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const CVButton = () => {
     return (
         <>
-            <a href='CV_MOUSSI_SID-AHMED.pdf' download="CV_MOUSSI_SID-AHMED.pdf">
+            <a href='CV_Moussi_Sid-Ahmed.pdf' download="CV_Moussi_Sid-Ahmed.pdf">
                 <button className='bg-teal-500 w-[160px] rounded-md my-6 mx-auto py-3 text-white font-bold italic lg:w-[200px]'>
                     Télécharger mon CV
                 </button>
